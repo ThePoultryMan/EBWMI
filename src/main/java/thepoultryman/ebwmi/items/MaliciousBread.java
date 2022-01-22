2fcfd777c7098458207007cbe1d72ce8dc333341
ThePoultryMan <kellingcolehi@gmail.com>
2022-01-21 17:14:40 -0800

    Tabs to spaces

--- src/main/java/thepoultryman/ebwmi/items/MaliciousBread.java
+++ src/main/java/thepoultryman/ebwmi/items/MaliciousBread.java
@@ -10,26 +10,26 @@ import thepoultryman.ebwmi.Ebwmi;
 import thepoultryman.ebwmi.registry.ItemRegistry;
 
 public class MaliciousBread extends Item {
-	public MaliciousBread(Settings settings) {
-		super(settings);
-	}
+    public MaliciousBread(Settings settings) {
+        super(settings);
+    }
 
-	@Override
-	public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-		if (entity instanceof LivingEntity livingEntity && selected)
-			livingEntity.addStatusEffect(new StatusEffectInstance(Ebwmi.MALICIOUS_INTENT_STATUS_EFFECT, 220));
-		else if (entity instanceof LivingEntity livingEntity)
-			livingEntity.removeStatusEffect(Ebwmi.MALICIOUS_INTENT_STATUS_EFFECT);
-	}
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (entity instanceof LivingEntity livingEntity && selected)
+            livingEntity.addStatusEffect(new StatusEffectInstance(Ebwmi.MALICIOUS_INTENT_STATUS_EFFECT, 220));
+        else if (entity instanceof LivingEntity livingEntity)
+            livingEntity.removeStatusEffect(Ebwmi.MALICIOUS_INTENT_STATUS_EFFECT);
+    }
 
-	@Override
-	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-		stack.damage(1, user, (userx) -> {
-			userx.sendToolBreakStatus(user.preferredHand);
-		});
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        stack.damage(1, user, (userx) -> {
+            userx.sendToolBreakStatus(user.preferredHand);
+        });
 
-		user.eatFood(world, new ItemStack(ItemRegistry.MALICIOUS_BREAD));
+        user.eatFood(world, new ItemStack(ItemRegistry.MALICIOUS_BREAD));
 
-		return stack;
-	}
+        return stack;
+    }
 }
