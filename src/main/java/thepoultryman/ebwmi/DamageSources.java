77410ca4fda04b367a9a8cb73f940c8dcfa9d66d
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-01-26 21:48:47 -0800

    Add extraction damage source

--- src/main/java/thepoultryman/ebwmi/DamageSources.java
+++ src/main/java/thepoultryman/ebwmi/DamageSources.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.damage.DamageSource;
 
 public class DamageSources extends DamageSource {
     public static final DamageSource SOUND = new DamageSources("sound");
+    public static final DamageSource EXTRACTOR = new DamageSources("extractor");
 
     protected DamageSources(String string) {
         super(string);
