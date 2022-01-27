19dca71aeac2501a7c0a4e2b2c15cbebfd52ece9
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-01-26 21:30:43 -0800

    Optimize imports

--- src/main/java/thepoultryman/ebwmi/EbwmiClient.java
+++ src/main/java/thepoultryman/ebwmi/EbwmiClient.java
@@ -1,12 +1,6 @@
 package thepoultryman.ebwmi;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
-import net.minecraft.client.render.RenderLayer;
-import thepoultryman.ebwmi.blockentityrenderers.CampfirePotBlockEntityRenderer;
-import thepoultryman.ebwmi.registry.BlockEntityRegistry;
-import thepoultryman.ebwmi.registry.BlockRegistry;
 import thepoultryman.ebwmi.registry.BlockRendererRegistry;
 
 public class EbwmiClient implements ClientModInitializer {
